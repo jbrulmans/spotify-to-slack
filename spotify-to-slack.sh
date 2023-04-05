a3c2bb9b2d0ce5f49aa640287576fd7882ae49cc
Jens Brulmans <jens.brulmans@roche.com>
2023-04-05 14:33:29 +0200

    Update to new slack token creation

--- spotify-to-slack.sh
+++ spotify-to-slack.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
-APIKEY="From Here https://api.slack.com/custom-integrations/legacy-tokens"
-EMOJI="headphones"
+# Get this token by:
+# - Creating a Slack app https://api.slack.com/apps
+# - Select Permissions > Scopes > User Token Scopes and add `users.profile:write`
+# - Scroll up and select Install to Workspace under OAuth Tokens for Your Workspace
+# - Copy User OAuth Token under OAuth Tokens for Your Workspace
+TOKEN=""
+EMOJI=":headphones:"
 trap onexit INT
 
 function reset() {
@@ -24,9 +28,12 @@ echo "Spotify: "$state
 if [[ "$state" != "playing" ]]; then
     reset
 else
-    SONG=$(osascript -e 'tell application "Spotify" to artist of current track & " - " & name of current track')
-    URLSONG=$(echo "$SONG" | perl -MURI::Escape -ne 'chomp;print uri_escape($_),"\n"')
-    echo $SONG
+    SONG=$(osascript -e 'tell application "Spotify" to name of current track')
+    ARTIST=$(osascript -e 'tell application "Spotify" to artist of current track')
+    echo $ARTIST - $SONG
 
-    curl -s -d "payload=$json" "https://slack.com/api/users.profile.set?token="$APIKEY"&profile=%7B%22status_text%22%3A%22"$URLSONG"%22%2C%22status_emoji%22%3A%22%3A$EMOJI%3A%22%7D"
+    JSON=$(echo '{}' | jq --arg SONG "$SONG" --arg ARTIST "$ARTIST" --arg EMOJI "$EMOJI" '.profile.status_text=$ARTIST+" - "+$SONG | .profile.status_emoji=$EMOJI')
+
+
+    curl -X POST --data "$JSON" -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json; charset=utf-8" --silent -o /dev/null https://slack.com/api/users.profile.set | jq 'del(.profile)'
 fi
